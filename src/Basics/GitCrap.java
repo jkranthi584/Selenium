d5e9e01c2b64365ab614753457b47590ab8f57e6
Nick <yesdreamsdocometrue3@gmail.com>
2018-08-04 18:52:41 +0530

    my conflict first try

---
+++ src/Basics/GitCrap.java
@@ -0,0 +1,14 @@
+package Basics;
+
+public class GitCrap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         int i=5;
+         int y=5,x=5,z=9,p=8;
+         for(int k=0; k<10; k++)
+        	 k=i;
+        	  
+	}
+
+}
