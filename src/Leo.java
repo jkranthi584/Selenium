b7a24421aacec908bc308e35e442cffbd406fa5f
Nick <yesdreamsdocometrue3@gmail.com>
2018-08-04 21:32:09 +0530

    this sucks

--- src/Leo.java
+++ src/Leo.java
@@ -1,4 +1,15 @@
 
 public class Leo {
+
  int i=5;
+
+
+	public static void m() {
+		System.out.println();
+		System.out.println("another change to recognise by the git diff");
+	}
+  public static void anotherMethond() {
+	  int f=999;
+	  System.out.println(f);
+  }
 }
