0e372092a46e57ce928cbaeb8bd664101d5951a0
Nick <yesdreamsdocometrue3@gmail.com>
2018-08-04 21:51:19 +0530

    updating again

--- src/Leo.java
+++ src/Leo.java
@@ -4,7 +4,25 @@ public class Leo {
  int i=5;
  int k=9;
 	int f=9999;
-
+/* 1 step) update the repo
+ * 2 step) update same program in eclipse anything but make sure not to override if
+ * you did overide something, then conflict will occur
+ * 3 step) check the status git status
+ * 4 step) git add * java
+ * 5 step) git stash save the local work make it invisible
+ * 6 step) git stash download the code from repo
+ * git pull origin master
+ * 7 step) now supply your local code git stash apply
+ * 8 step) git diff src/leo.java see the modification
+ * see what was already in the repo and what you have added 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * */
 
 	public static void m() {
 		System.out.println();
@@ -14,9 +32,11 @@ public class Leo {
 	  int f=999;
 	  System.out.println(f);
 	  System.out.println("i like rock");
+	  System.out.println("simple cone");
   }
   public static void anotherMethonddfd() {
 	   System.out.println("care");
+	   System.out.println("now check the status");
   }
 	private static void my(){
 		String f ="joker";
